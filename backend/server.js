ea160eb5d9194f79a42c25bb051649c121c392c6
kittori123@gmail.com <kittori123@gmail.com>
2024-09-13 20:06:26 +0700

    fixing server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -18,18 +18,6 @@ mongoose.connect('mongodb://localhost:27017')
     console.log('Failed to connect to MongoDB', err);
   });
 
-const fishRoutes = require('./src/routes/fishRoutes');
-app.use('/fishs', fishRoutes);
-
-const accRoutes = require('./src/routes/accRoutes');
-app.use('/accounts', accRoutes);
-
-const loginRoute = require('./src/routes/loginRoutes');
-app.use('/login', loginRoute);
-
-const cartRoute = require('./src/routes/cartRoutes');
-app.use('/carts', cartRoute);
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
